a93e209f97e3aeefb377034471a3fff28d64269e
gnoanto94 <gnoanto94@gmail.com>
2016-01-23 14:05:37 +0100

    Modificato Parzialmente Panel VisualizzaIncasso

--- PROGETTO 2015-16/src/graphics/testing/VisualizzaIncassoPanel.java
+++ PROGETTO 2015-16/src/graphics/testing/VisualizzaIncassoPanel.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -21,8 +22,12 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 	public VisualizzaIncassoPanel(StrutturaSportiva struct){
 		this.struct = struct;
 		
+		
+		if(this.struct == null){
+			throw new NullPointerException("La struttura sportiva non è presente !!!");
+		}
+		
 		//Metodi di supporto
-		this.createComboStadi();
 		this.createElements();
 		this.createButton();
 		
@@ -30,26 +35,27 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 		this.add(sceltaPanel, BorderLayout.NORTH);
 		this.add(labelIncasso, BorderLayout.CENTER);
 		
+		this.setSize(300, 200);
+		
 		
 	}
 	
 	public void createComboStadi(){
-		// da modificare
 		this.selezionaStadio = new JComboBox<Stadio>(this.struct.getStadi().toArray(new Stadio[this.struct.getStadi().size()]));
-		
+		this.selezionaStadio.setSelectedIndex(0);
 	}
 	
 	public void createElements(){
 		ButtonGroup gruppoRadio = new ButtonGroup();
 		this.radioTotale = new JRadioButton("Totale", true);
 		
-		this.radioTotale.addActionListener(new ActionListener() {
+		/*this.radioTotale.addActionListener(new ActionListener() {
 		
 			@Override
 			public void actionPerformed(ActionEvent e){
 				selezionaStadio.setEnabled(false);
 			}
-		});
+		});*/
 		
 		this.radioPerStadio = new JRadioButton("per Stadio");
 		
@@ -57,7 +63,12 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				selezionaStadio.setEnabled(true);
+				if(struct.getStadi().size() == 0){
+					throw new IllegalArgumentException("Attenzione, non sono presenti stadi !!!");
+				}
+				createComboStadi();
+				sceltaPanel.add(selezionaStadio);
+				revalidate();
 			}
 			
 			
@@ -69,7 +80,6 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 		this.sceltaPanel = new JPanel();
 		this.sceltaPanel.add(radioTotale);
 		this.sceltaPanel.add(radioPerStadio);
-		this.sceltaPanel.add(selezionaStadio);
 
 		this.labelIncasso = new JLabel();
 	}
@@ -83,7 +93,7 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 			public void actionPerformed(ActionEvent e) {
 				if(radioTotale.isSelected()){
 					labelIncasso.setText("" + struct.calcolaIncasso(struct.getAcquisti()));
-				} else {
+				} else if(radioPerStadio.isSelected()){
 					labelIncasso.setText("" + struct.calcolaIncasso(struct.getAcquistiFiltrati(new PurchasesByStadium((Stadio) selezionaStadio.getSelectedItem()))));
 				}
 				
@@ -103,6 +113,20 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 	private JLabel labelIncasso;
 	private JButton visualizzaButton;
 	private StrutturaSportiva struct;
+	
 	private static final long serialVersionUID = -4738376842736705159L;
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Test");
+		
+		StrutturaSportiva prova = new StrutturaSportiva("Prova");
+		prova.addStadio(new Stadio("Olimpico", 50000, 20.00));
+		frame.add(new VisualizzaIncassoPanel(prova));
+		
+		frame.setSize(500, 200);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 
 }
