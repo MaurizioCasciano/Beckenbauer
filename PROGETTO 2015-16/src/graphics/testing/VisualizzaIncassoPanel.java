5052fbffe64781be57ed3628753b078c358865b6
gnoanto94 <gnoanto94@gmail.com>
2016-01-23 16:07:30 +0100

    Completato Panel VisualizzaIncasso

--- PROGETTO 2015-16/src/graphics/testing/VisualizzaIncassoPanel.java
+++ PROGETTO 2015-16/src/graphics/testing/VisualizzaIncassoPanel.java
@@ -1,19 +1,19 @@
 package graphics.testing;
 
-import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.Serializable;
 
 import javax.swing.ButtonGroup;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.SwingConstants;
 
 import struttura.Stadio;
 import struttura.StrutturaSportiva;
@@ -29,49 +29,78 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 			throw new NullPointerException("La struttura sportiva non è presente !!!");
 		}
 		
+		this.radioPanel = new JPanel();
+		this.comboButtonPanel = new JPanel();
+		
 		//Metodi di supporto
-		this.createButton();
 		this.createElements();
 		
-		this.setLayout(new BorderLayout());
-		this.add(sceltaPanel, BorderLayout.NORTH);
-		this.add(labelIncasso, BorderLayout.CENTER);
+		this.setLayout(new GridLayout(3, 1, 3, 3));
+		JPanel topPanel = new JPanel();
+		topPanel.add(radioPanel);
+		topPanel.add(comboButtonPanel);
+		
+		this.titolo = new JLabel("Visualizza Incasso");
+		this.titolo.setHorizontalAlignment(JLabel.CENTER);
+		this.titolo.setFont(new Font(titolo.getFont().getName(), Font.BOLD, 18));
+		
+		this.add(titolo);
+		this.add(topPanel);
+		this.add(labelIncasso);
 		
 		this.setSize(300, 200);
 		
 		
 	}
 	
-	public void createComboStadi(){
-		this.selezionaStadio = new JComboBox<Stadio>(this.struct.getStadi().toArray(new Stadio[this.struct.getStadi().size()]));
-		this.selezionaStadio.setSelectedIndex(0);
+	private void createComboStadi(){
+		this.comboStadi = new JComboBox<Stadio>(this.struct.getStadi().toArray(new Stadio[this.struct.getStadi().size()]));
+		this.comboStadi.setSelectedIndex(0);
+		
+		this.comboStadi.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == ItemEvent.SELECTED){
+					labelIncasso.setText("Incasso: " + struct.calcolaIncasso(struct.getAcquistiFiltrati(new PurchasesByStadium((Stadio) comboStadi.getSelectedItem()))));
+				}
+				
+			}
+			
+		});
 	}
 	
-	public void createElements(){
+	private void createElements() throws IllegalArgumentException{
 		ButtonGroup gruppoRadio = new ButtonGroup();
 		this.radioTotale = new JRadioButton("Totale", true);
+		this.radioTotale.setFont(new Font(radioTotale.getFont().getName(), Font.PLAIN, 16));;
 		
-		/*this.radioTotale.addActionListener(new ActionListener() {
+		this.radioTotale.addActionListener(new ActionListener() {
 		
 			@Override
 			public void actionPerformed(ActionEvent e){
-				selezionaStadio.setEnabled(false);
+				comboButtonPanel.removeAll();  // bisogna eseguire il removeAll() e il revalidate() per rimuovere la combo
+				comboButtonPanel.revalidate();
+				labelIncasso.setText("Incasso: " + struct.calcolaIncasso(struct.getAcquisti()));
 			}
-		});*/
+		});
 		
 		this.radioPerStadio = new JRadioButton("per Stadio");
+		this.radioPerStadio.setFont(new Font(radioPerStadio.getFont().getName(), Font.PLAIN, 16));
 		
 		this.radioPerStadio.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				
 				if(struct.getStadi().size() == 0){
 					throw new IllegalArgumentException("Attenzione, non sono presenti stadi !!!");
 				}
 				createComboStadi();
-				sceltaPanel.add(selezionaStadio);
-				sceltaPanel.add(visualizzaButton);
-				revalidate();
+				comboButtonPanel.removeAll();
+				comboButtonPanel.add(comboStadi);
+				comboButtonPanel.revalidate();
+				labelIncasso.setText("Incasso: " + struct.calcolaIncasso(struct.getAcquistiFiltrati(new PurchasesByStadium((Stadio) comboStadi.getSelectedItem()))));
 			}
 			
 			
@@ -80,43 +109,26 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 		gruppoRadio.add(radioTotale);
 		gruppoRadio.add(radioPerStadio);
 		
-		this.sceltaPanel = new JPanel();
-		this.sceltaPanel.add(radioTotale);
-		this.sceltaPanel.add(radioPerStadio);
-		this.sceltaPanel.add(visualizzaButton);
+		
+		this.radioPanel.add(radioTotale);
+		this.radioPanel.add(radioPerStadio);
 
 		this.labelIncasso = new JLabel();
-		this.labelIncasso.setHorizontalAlignment(SwingConstants.CENTER);
+		this.labelIncasso.setHorizontalAlignment(JLabel.CENTER);
 		this.labelIncasso.setFont(new Font(labelIncasso.getFont().getName(), Font.BOLD, 20));
-
-	}
-	
-	public void createButton(){
-		this.visualizzaButton = new JButton("Visualizza");
 		
-		this.visualizzaButton.addActionListener(new ActionListener(){
+		this.radioTotale.doClick();
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(radioTotale.isSelected()){
-					labelIncasso.setText("" + struct.calcolaIncasso(struct.getAcquisti()));
-				} else if(radioPerStadio.isSelected()){
-					labelIncasso.setText("" + struct.calcolaIncasso(struct.getAcquistiFiltrati(new PurchasesByStadium((Stadio) selezionaStadio.getSelectedItem()))));
-				}
-				
-			}
-			
-			
-		});
 	}
 	
 	
-	private JPanel sceltaPanel;
+	private JPanel radioPanel;
+	private JPanel comboButtonPanel;
 	private JRadioButton radioTotale;
 	private JRadioButton radioPerStadio;
-	private JComboBox<Stadio> selezionaStadio;
+	private JComboBox<Stadio> comboStadi;
+	private JLabel titolo;
 	private JLabel labelIncasso;
-	private JButton visualizzaButton;
 	private StrutturaSportiva struct;
 	
 	private static final long serialVersionUID = -4738376842736705159L;
@@ -126,6 +138,7 @@ public class VisualizzaIncassoPanel extends JPanel implements Serializable{
 		
 		StrutturaSportiva prova = new StrutturaSportiva("Prova");
 		prova.addStadio(new Stadio("Olimpico", 50000, 20.00));
+		prova.addStadio(new Stadio("San Siro", 30000, 30.00));
 		frame.add(new VisualizzaIncassoPanel(prova));
 		
 		frame.setSize(500, 200);
